47972be1c17112ec8dff9bc66e43fbf7c711054c
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2024-08-29 10:58:24 +0100

    check if user is authenticate and Implement userDetailsService

--- security-service/src/main/java/com/mohamed/securityservice/security/JwtFilter.java
+++ security-service/src/main/java/com/mohamed/securityservice/security/JwtFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +18,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
+    private final UserDetailsService userDetailsService;
     private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
@@ -27,8 +31,13 @@ public class JwtFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
+
         String jwt=authHeader.substring(7);
-        String username=jwtService.extractUsername(jwt);
+        String userEmail=jwtService.extractUsername(jwt);
+
+        if(userEmail !=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails =userDetailsService.loadUserByUsername(userEmail);
+        }
 
     }
 }
